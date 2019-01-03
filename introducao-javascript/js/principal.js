fd9eb77c57d9173530409c34b6735f9b7b856c78
Rodrigo Felix <rodrigo.felix321@gmail.com>
2019-01-02 22:12:20 -0200

    Fim da aula 02 - Buscando e enviando informações com o JavaScript

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,35 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = document.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if(peso <=0 || peso > 100) {
+    console.log("Peso Inválido");
+    tdImc.textContent = "Peso Inválido!";
+    pesoEhValido = false;
+}
+
+if(altura <=0 || altura > 3.00) {
+    console.log("Altura Inválida");
+    tdImc.textContent = "Altura Inválida!";
+    alturaEhValida = false;
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
